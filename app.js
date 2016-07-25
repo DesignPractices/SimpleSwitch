bef6aadd1c807653ee7b415fb9e5fee408b8c66b
Vineeth N V <vineeth1991@gmail.com>
2016-07-25 23:50:44 +0530

    Color change

--- app.js
+++ app.js
@@ -3,8 +3,11 @@
 var ViewController = (function ($) {
     $(function () {
         var $button = $('.switch');
+        var $colorOverlay = $('.color_overlay');
         $button.on('click', function () {
-            $button.toggleClass('toggle');
+            $button.toggleClass('toggle', function() {
+                $colorOverlay.css({"background-color", "rgba(67, 215, 147, 1)"})
+            });
         });
     });
 })(jQuery);
