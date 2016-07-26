24528e5082fdc90b643bd9a0bff1ec8a79118e58
Vineeth N V <vineeth1991@gmail.com>
2016-07-26 23:50:58 +0530

    Few changes in classed and js

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 'use strict';
 
 var ViewController = (function ($) {
-    $(function () {
-        var $button = $('.switch');
-        var $colorOverlay = $('.color_overlay');
-        $button.on('click', function () {
-            $button.toggleClass('toggle', function() {
-                $colorOverlay.css({"background-color", "rgba(67, 215, 147, 1)"})
-            });
-        });
-    });
+	$(function () {
+		var $switch = $('.switch_cta');
+		var $statusWrapper = $('.status_wrapper');
+        var $text = $('.text');
+        $switch.on('click', function() {
+			$switch.toggleClass('toggle');
+			$statusWrapper.toggleClass('status_change');
+            if ($switch.hasClass('toggle')) {
+                $text.html('On')
+            } else {
+                $text.html('Off')
+            }
+		})
+	});
 })(jQuery);
